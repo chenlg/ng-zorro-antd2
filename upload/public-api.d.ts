15db6dbc444fc931a9547c0876030a36b0ca9cbd
wikihost <30051483@qq.com>
2019-03-12 18:28:39 +0800

    d

--- upload/public-api.d.ts
+++ upload/public-api.d.ts
@@ -3,4 +3,3 @@ export * from './nz-upload-btn.component';
 export * from './nz-upload-list.component';
 export * from './nz-upload.component';
 export * from './nz-upload.module';
-export * from './interface';
